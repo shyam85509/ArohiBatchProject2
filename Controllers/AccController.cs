02ad7193294a304f90978cfd5494a1ad800d3d76
Shyam <shyam85509@gmail.com>
2024-01-26 12:57:26 +0530

    updated

---
+++ Controllers/AccController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gitproject.Controllers
+{
+    public class AccController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
